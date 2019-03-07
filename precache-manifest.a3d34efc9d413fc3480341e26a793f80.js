cf23fe1ea219212203c6bc32f2662a26c6736c31
SahibSinghBanga <sahib_singh_1997@hotmail.com>
2019-03-07 20:14:36 +0530

    Updates

---
+++ precache-manifest.a3d34efc9d413fc3480341e26a793f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bd4ffe05b9c1f810bb6c",
+    "url": "/static/js/main.bd4ffe05.chunk.js"
+  },
+  {
+    "revision": "56b3e6c913916fbf857d",
+    "url": "/static/js/2.56b3e6c9.chunk.js"
+  },
+  {
+    "revision": "bd4ffe05b9c1f810bb6c",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "56b3e6c913916fbf857d",
+    "url": "/static/css/2.f4b1eaa9.chunk.css"
+  },
+  {
+    "revision": "a0636331de3eeb1b8670f522d6f64a73",
+    "url": "/index.html"
+  }
+];
